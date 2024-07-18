02bfa335756583686883fb9d297d5d73558f5a81
Chandu_Kommineni <chandukommineni63@gmail.com>
2024-07-18 11:23:21 +0530

    frontend setup is done

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { useSelector } from 'react-redux';
+const App = () => {
+ 
+  return <div className="bg-blue-500 text-3xl">hello</div>;
+};
+
+export default App;
