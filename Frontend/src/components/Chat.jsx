df51c5ae15117efa55c25b9cc8472d979fd6ef19
Chandu_Kommineni <chandukommineni63@gmail.com>
2024-07-19 17:56:00 +0530

    quiz completed

--- Frontend/src/components/Chat.jsx
+++ Frontend/src/components/Chat.jsx
@@ -1,7 +1,26 @@
 import React from "react";
-
+import { MdSend } from 'react-icons/md';
+import Upload from "./Upload";
 const Chat = () => {
-  return <div>Chat</div>;
+  return(
+    <div className="flex h-full w-full justify-evenly pt-7">
+      <div className="bg-gray-800 h-5/6 w-3/4 border-gray-50 rounded-xl">
+          <div style={{height:"88%"}}>
+
+          </div>
+          <div style={{height:"10%",border:"2px solid #f76d7d",width:"100%"}} className="mt-2 rounded-xl flex" >
+
+            <input type="text" className=" h-full bg-transparent outline-none text-white pl-4 pr-4" style={{width:"90%"}} placeholder="Enter your Query ?" />
+            <MdSend className="md:ml-10 text-3xl text-center mt-2  cursor-pointer" style={{color:"#f76d7d"}}/>
+            
+
+          </div>
+
+          </div>
+      <Upload/>
+      
+    </div>
+  )
 };
 
 export default Chat;
--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -1,22 +1,22 @@
 import React from "react";
+import Typewriter from "./TypeWriter";
 
 const Home = () => {
   return(
-    <div className="h-screen pr-4 mt-10 md:w-2/3">
+    <div className="h-screen pr-4 pt-10 md:w-2/3">
       <div className="bg-gray-800 flex flex-col justify-evenly align-middle p-5 rounded-2xl h-56 ">
         <div className="text-white font-semibold text-4xl pl-5 pt-5">
-            <h1 >Start Learning</h1>
-            <h1  >Choose your Companion</h1>
+            <h1 >Welcome to Student Genie</h1>
+            <h1  className="text-2xl">Empower Your Learning with Intelligent Tools</h1>
         </div>
         
-        <div className="bg-customRed text-white w-32 h-8 text-lg rounded-lg text-center">
+        <div className="bg-customRed text-white w-32 h-8 text-lg rounded-lg text-center ml-4">
           Get Started
         </div>
 
       </div>
-      <div className="bg-gray-800 p-5 rounded-2xl h-60 md:w-3/5 mt-7 ">
-        <h2>HEll0</h2>
-
+      <div className="bg-gray-800 p-5 rounded-2xl h-60 max-h-72 md:w-4/5 mt-7  text-white text-lg ">
+         <Typewriter/>
       </div>
     </div>
   )
